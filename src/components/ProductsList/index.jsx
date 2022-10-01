1923929cde6242e34fe26e92b8fd732f386a8ccd
Pedro Henrique Feitosa <50965091+bobjoe159@users.noreply.github.com>
2022-09-30 22:25:44 -0300

    fix: corrigido delay animação lista de produtos

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ProductCard from '../ProductCard'
 import { ProductsList } from './style'
 import 'animate.css'
 
 export default function ProductList({products, cartItem, setCartItem}) {
   return (
-    <ProductsList className='animate__animated animate__fadeIn'> 
+    <ProductsList className='animate__animated animate__fadeIn animate__delay-2s'> 
       {
         products.map(product => 
           <ProductCard key={product.id} product={product} products={products} cartItem={cartItem} setCartItem={setCartItem} />
